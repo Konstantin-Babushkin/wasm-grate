d7cfa68624d7921f2efe569785fa1c1ddc9d850a
Konstantin-Babushkin <constant-babushkin@yandex.ru>
2023-11-04 11:59:46 +0300

    release: build v0.1.19

--- pre-install.js
+++ pre-install.js
@@ -29,8 +29,8 @@ if (fs.existsSync(cargoDir)) {
     
 const features = process.env.npm_config_features ? `--features ${process.env.npm_config_features.replace(",", " ")}` : ""; 
 
-console.log(`Installing and compiling wasm-grate 0.1.0 ${features} ...`);
-exec(`cargo install wasm-grate --vers 0.1.0 ${features}`, (error, stdout, stderr) => {
+console.log(`Installing and compiling wasm-grate 0.1.19 ${features} ...`);
+exec(`cargo install wasm-grate --vers 0.1.19 ${features}`, (error, stdout, stderr) => {
   console.log(stdout);
   if (error || stderr) {
     console.log(error || stderr);
